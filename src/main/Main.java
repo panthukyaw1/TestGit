9f6a8c7519e86cf62a9249b531c635f7341b62ca
MgMg <soelinnaung.101@gmail.com>
2022-10-21 12:21:44 +0630

    Test SSH Success

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git Version Control");
 		System.out.println("Hello Git Version Control-Changed1");
+		System.out.println("Test SSH Success");
 	}
 
 }
